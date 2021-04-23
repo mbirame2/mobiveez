2cc18b8b7b2824893250d316bda054daec7e13f6
mbirame2 <mbirame2000@gmail.com>
2021-04-23 01:27:08 +0000

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('statut:update')->daily()->at('01:05');
+        $schedule->command('statut:update')->daily()->at('01:28');
         
     }
 
