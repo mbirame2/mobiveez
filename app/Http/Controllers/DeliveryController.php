4b5a49e0708c270dd2f1298318b0364f9c392ff9
Birame MBOUP <bmboup@peopleinput.com>
2023-06-21 11:42:52 +0000

    fix bugs

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -12,9 +12,9 @@ class DeliveryController extends Controller
 {
     
     public function getZone($country){
-        $zone = zone::whereHas('departement', function ($query) {
+        $zone = zone::whereHas('departement', function ($query)  {
                     $query->whereHas('region', function ($query) {
-                        $query->whereHas('pays', function ($query) {
+                        $query->whereHas('pays', function ($query) use ($country) {
                             $query->where('lib_pays', $country);
                         });
                     });
